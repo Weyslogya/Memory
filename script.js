b5a6701192445a5dfc10b0fdae8be02d282290e5
Weyslogya <weyslogya@hotmail.com>
2023-05-25 15:50:31 +0200

    mise en place de la limite de carte

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
 const cartes = document.querySelectorAll(".carte");
 
 let carteUne, carteDeux;
+let limiteCarte = false;
 
-function resetCarte () {
-    if (carteDeux) {
-        carteUne = undefined;
-        carteDeux = undefined;
-    }
+function resetCarte() {
+  if (carteDeux) {
+    carteUne = undefined;
+    carteDeux = undefined;
+  }
 }
 
-function flipCarte(event){
-    const carte = event.target.closest('.carte');
+function flipCarte(event) {
+  if (limiteCarte) {
+    return;
+  }
 
-    let carteFlippée = carte.className.includes('flip');
-    if (carteFlippée) {
-        return;
-    }
+  const carte = event.target.closest(".carte");
 
-    carte.classList.add("flip");
+  let carteFlippée = carte.className.includes("flip");
+  if (carteFlippée) {
+    return;
+  }
 
-    if (carteUne) {
-        carteDeux = carte;
-    } else {
-        carteUne = carte;
-    }
+  carte.classList.add("flip");
 
-    if (!carteDeux) {
-        return;
-    }
-
-    if (carteUne.dataset.paire === carteDeux.dataset.paire){
-        carteUne.removeEventListener("click", flipCarte);
-        carteDeux.removeEventListener("click", flipCarte);
-        resetCarte();
-    } else {
-        setTimeout (() =>{
-            carteUne.classList.remove("flip");
-            carteDeux.classList.remove("flip");
-            resetCarte();
-        },1500)
-    }
-
+  if (carteUne) {
+    carteDeux = carte;
+  } else {
+    carteUne = carte;
+  }
+  matching();
 }
-cartes.forEach(carte => carte.addEventListener("click",flipCarte));
+
+function matching() {
+  if (!carteDeux) {
+    return;
+  }
+
+  if (carteUne.dataset.paire === carteDeux.dataset.paire) {
+    carteUne.removeEventListener("click", flipCarte);
+    carteDeux.removeEventListener("click", flipCarte);
+    resetCarte();
+  } else {
+    limiteCarte = true;
+    setTimeout(() => {
+      carteUne.classList.remove("flip");
+      carteDeux.classList.remove("flip");
+      resetCarte();
+      limiteCarte = false;
+    }, 1500);
+  }
+}
+cartes.forEach((carte) => carte.addEventListener("click", flipCarte));
 
 /*  Faire une fonction bloquant plus de 2 carte
     Faire une fonction pour le bouton jouer
-    Faire une fonction pour mélanger les cartes*/ 
+    Faire une fonction pour mélanger les cartes*/
